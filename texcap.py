e4cf7f0670606e398e788bc01a3dc6a68511724d
Zerix <onlyonesewon@outlook.com>
2021-05-14 22:42:10 +0900

    Update texcap.py
    add header

--- texcap.py
+++ texcap.py
@@ -1,1 +1,13 @@
-print("hello");
+import glob
+import os
+import random
+import sysconfig
+import random
+import math
+import json
+from collections import defaultdict
+
+import cv2
+from PIL import image, ImageDraw
+import numpy as np
+from scipy.ndimage.filters import rank_filter
